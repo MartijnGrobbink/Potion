e6b8325deb6350dd29d05dcbae2aa1a0fdbd9f00
Shadowfox3 <48592206+Shadowfox3@users.noreply.github.com>
2021-11-07 17:14:43 +0100

    potion paticles and potion duration damage

--- Potion/Assets/Potion.cs
+++ Potion/Assets/Potion.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 
 public class Potion : MonoBehaviour
 {
+    Rigidbody potionRigid;
+
+    public GameObject PotionSprite;
+    public GameObject AreaOfEffect;
+
+    public float damage;
+    public float duration;
+
+    bool broken;
     // Start is called before the first frame update
     void Start()
     {
-        
+        potionRigid = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(broken == true)
+        {
+            PotionSprite.SetActive(false);
+            AreaOfEffect.SetActive(true);
+
+            potionRigid.constraints = RigidbodyConstraints.FreezePosition;
+            potionRigid.constraints = RigidbodyConstraints.FreezeRotation;
+            duration -= Time.deltaTime;
+        }
+        if(duration <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag != "Player" && collision.gameObject.tag != "AOE")
         {
-            Destroy(gameObject);
+            broken = true;
         }  
     }
+    //void OnTriggerStay(Collision other)
+    //{
+        //if(other.gameObject.tag)
+    //}
 }
